bf1f4b5925aebd96db8bbfc0cb89cd567512e0f4
Yudi <Yudi__Zhang@outlook.com>
2024-10-29 16:47:25 +0800

    resource

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import '@/styles/common.scss'
 
 //测试
 import {getCategory} from '@/apis/testAPI'
@@ -12,6 +13,7 @@ getCategory().then(res => {
     console.log(res)
 })
 const app = createApp(App)
+
 
 app.use(createPinia())
 app.use(router)
