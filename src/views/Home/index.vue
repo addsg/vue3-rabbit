458ecdda89cb1c98825da1083c2ba414fa62a1c0
Yudi <Yudi__Zhang@outlook.com>
2024-10-30 14:40:25 +0800

    home-整体结构

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,18 @@
-<template>
-    我是home
-    <div style="height: 1200px;">
+<script setup lang="ts">
+import HomeCategory from './components/HomeCategory.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomeBanner from './components/HomeBanner.vue'
+</script>
 
+<template>
+    <div class="container">
+      <HomeCategory/>
+      <HomeBanner/>
     </div>
+    <HomeNew/>
+    <HomeHot/>
+    <HomeProduct/>
 </template>
+  
