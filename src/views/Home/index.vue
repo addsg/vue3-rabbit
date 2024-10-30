35b89a7c90d3b6a5417a8373a934ff3e48412f51
Yudi <Yudi__Zhang@outlook.com>
2024-10-30 16:11:40 +0800

    新鲜好物

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeHot from './components/HomeHot.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeProduct from './components/HomeProduct.vue'
 import HomeBanner from './components/HomeBanner.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,5 +15,14 @@ import HomeBanner from './components/HomeBanner.vue'
     <HomeNew/>
     <HomeHot/>
     <HomeProduct/>
+    <HomePanel title="新鲜好物" sub-title="好多商品">
+        <div>
+            插槽
+        </div>
+    </HomePanel>
+
+    <HomePanel title="人气好物" sub-title="好多商品">
+        
+    </HomePanel>
 </template>
   
