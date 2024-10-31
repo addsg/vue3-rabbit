85d09aec26c96dbc08e7134e70aaeb381dc6071a
Yudi <Yudi__Zhang@outlook.com>
2024-10-31 14:09:36 +0800

    激活状态显示以及分类

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,7 +3,7 @@ import { getCategoryAPI } from '@/apis/category';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { getBannerAPI } from '@/apis/home';
-
+import GoodsItem from '../Home/components/GoodsItem.vue';
 const categoryData = ref({})
 const route = useRoute()
 const getCategory = async () => {
@@ -41,6 +41,25 @@ onMounted(() => getBanner())
                         <img :src="item.imgUrl" alt="">
                     </el-carousel-item>
                 </el-carousel>
+            </div>
+            <div class="sub-list">
+                <h3>全部分类</h3>
+                <ul>
+                    <li v-for="i in categoryData.children" :key="i.id">
+                        <RouterLink to="/">
+                            <img :src="i.picture" />
+                            <p>{{ i.name }}</p>
+                        </RouterLink>
+                    </li>
+                </ul>
+            </div>
+            <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+                <div class="head">
+                    <h3>- {{ item.name }}-</h3>
+                </div>
+                <div class="body">
+                    <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+                </div>
             </div>
         </div>
     </div>
@@ -125,13 +144,15 @@ onMounted(() => getBanner())
         padding: 25px 0;
     }
 }
+
 .home-banner {
-  width: 1240px;
-  height: 500px;
-  margin: 0 auto;
-  img {
-    width: 100%;
+    width: 1240px;
     height: 500px;
-  }
+    margin: 0 auto;
+
+    img {
+        width: 100%;
+        height: 500px;
+    }
 }
 </style>
