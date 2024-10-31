57da59ab2c0f92d89cd705c21c4c8d11cb9633ce
Yudi <Yudi__Zhang@outlook.com>
2024-10-31 15:40:36 +0800

    一级分类-业务逻辑拆分

---
+++ src/views/Category/composables/useCategory.js
@@ -0,0 +1,22 @@
+import { getCategoryAPI } from '@/apis/category';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import {onBeforeRouteUpdate} from 'vue-router'
+export function useCategory() {
+    const categoryData = ref({})
+    const route = useRoute()
+    const getCategory = async (id = route.params.id) => {
+        const res = await getCategoryAPI(id)
+        categoryData.value = res.result
+    }
+    onMounted(() => getCategory())
+
+    onBeforeRouteUpdate((to) => {
+        console.log("路由变化")
+        console.log(to)
+        getCategory(to.params.id)
+    })
+    return {
+        categoryData
+    }
+}
