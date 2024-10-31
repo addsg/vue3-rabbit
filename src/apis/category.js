89ba64a39515ccefebe79c2baee6309c6fd1766c
Yudi <Yudi__Zhang@outlook.com>
2024-10-31 16:38:48 +0800

    二级分类导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -9,3 +9,11 @@ export function getCategoryAPI(id) {
         } 
     })
 }
+export const getCategoryFilterAPI = (id) => {
+    return request({
+      url:'/category/sub/filter',
+      params:{
+        id
+      }
+    })
+  }
